35c5bd8c38c5f7e431214a1d7bebd90ac760f780
Marco <marco.escobar@ucb.edu.bo>
2022-03-28 22:34:36 -0400

    Implementando ,

--- calculadoraCadena.js
+++ calculadoraCadena.js
@@ -1,8 +1,14 @@
 
 function calcularCadena(palabra){ 
+    var resp = parseInt(palabra);
     if(palabra=="")
-        return 0;       
-    return parseInt(palabra);        
+        return 0;      
+    for(var i = 0;i<palabra.length;i++){
+        if(palabra[i]==","){
+            resp = resp + parseInt(palabra[i+1]);
+        }
+    }
+    return resp;        
 }
 
 export default calcularCadena;
--- calculadoraCadena2.test.js
+++ calculadoraCadena2.test.js
@@ -7,4 +7,7 @@ describe("Calculadora Cadena 2", () => {
   it("Tomando en cuenta una cadena 2 debe retornar 2", () => {       
     expect(calcularCadena("2")).toEqual(2);
   });
+  it("Tomando en cuenta una cadena 1,2 debe retornar 3", () => {       
+    expect(calcularCadena("1,2")).toEqual(3);
+  });
 });
